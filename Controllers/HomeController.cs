70acae15ca3eb2f416057a2d6f80f1d4d6db6732
ABRIL VARELA SICA <48464274@ort>
2024-08-07 11:40:23 -0300

    Terminado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,8 +38,6 @@ public class HomeController : Controller
     }
     public IActionResult Entretenimiento()
     {
-        ViewBag.ListaFotos = Elecciones.ListaFotos;
-        ViewBag.ListaBio = Elecciones.ListaBio;
         return View();
     }
    
--- Models/Elecciones.cs
+++ Models/Elecciones.cs
@@ -3,7 +3,7 @@ public static class Elecciones{
     public static List< string > ListaTitulos {get; set;} = new List<string>{"Hoteles", "Entretenimiento","Restaurantes"};
 
     public static List < string > ListaFotos {get; set;} = new List<string>{"HotelesPreview.png", "EntretenimientoPreview.png","RestaurantesPreview.png", 
-    "hotelpreview1.jpg", "hotelpreview2.jpg", "hotelpreview3.jfif", };
+    "hotelpreview1.jpg", "hotelpreview2.jpg", "hotelpreview3.jfif", "rprev1.jfif", "rprev2.jpg", "rprev3.jpg"};
 
     public static List < string > ListaBio {get;  set;} = new List<string>{"Ozen Hotel Bolifushi-Luxury All Inclusive",
      "Buenos Aires City Tour Privado con Guía",
@@ -11,9 +11,9 @@ public static class Elecciones{
      "The Ritz-Carlton, Hong Kong, China",
      "Ikos Andalusía, Estepona, España",
      "Romance Istanbul Hotel, Estambul, Turquía",
-     "R",
-     "R",
-     "R"
+     "El Mundo Genk, Italia", 
+     "Asador Etxebarri, Atxondo, España", 
+     "Table by Bruno Verjus, París, Francia"
     };
 
 }
